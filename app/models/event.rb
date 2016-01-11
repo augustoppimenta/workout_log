71f0d4bf5651935096c0c1729497aa03d599506f
Augusto Pimenta <augustoppimenta@gmail.com>
2016-01-11 20:43:15 -0300

    Create validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+
+  validates_presence_of :period, message: "deve ser preenchido"
+  validates_presence_of :type, message: "deve ser preenchido"
+  validates_presence_of :morning, message: "deve ser preenchido"
+
+  validates_numericality_of :period, greater_than: 0, message: "deve ser um maior que 0"
 end
