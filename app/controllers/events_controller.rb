2bce601de0eb1f7aa4d51697b0a2d21507888dc1
Augusto Pimenta <augustoppimenta@gmail.com>
2016-01-11 20:45:14 -0300

    Error sintax

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,17 @@ class EventsController < ApplicationController
   def index
 
   end
+
+
+  def create
+    @
+  end
+
+
+  private
+
+  def event_params
+    params.requite(:event).permit(:period, :title, :morning)
+
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
 
   validates_presence_of :period, message: "deve ser preenchido"
-  validates_presence_of :type, message: "deve ser preenchido"
+  validates_presence_of :title, message: "deve ser preenchido"
   validates_presence_of :morning, message: "deve ser preenchido"
 
   validates_numericality_of :period, greater_than: 0, message: "deve ser um maior que 0"
