18e8bcb72bfd747131d7979dac430b1f5a37aa13
Augusto Pimenta <augustoppimenta@gmail.com>
2016-01-11 21:00:08 -0300

    Create & Edit views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,30 @@
 class EventsController < ApplicationController
-
+  before_action :set_event, only: [:destroy]
   def index
-
+    @events = Event.all.order('created_at DESC')
   end
 
 
   def create
-    @
+    @event = Event.new(event_params)
+
+    @event.save
+    redirect_to events_path
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
   end
 
 
   private
 
   def event_params
-    params.requite(:event).permit(:period, :title, :morning)
+    params.require(:event).permit(:period, :title, :morning)
+  end
 
+  def set_event
+      @event = Event.find(params[:id])
   end
 end
